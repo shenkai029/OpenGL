29a0ff4be66b6296c6f49b0ef752a50f6f84ce46
Kai Shen <shenkai029@gmail.com>
2022-01-30 10:01:04 -0800

    Add batch rendering implementation instead of making multiple Draw call

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -50,12 +50,19 @@ int main(void)
          -50.0f,  -50.0f, 0.0f, 0.0f,// 0
           50.0f,  -50.0f, 1.0f, 0.0f,// 1
           50.0f,   50.0f, 1.0f, 1.0f,// 2
-         -50.0f,   50.0f, 0.0f, 1.0f// 3
+         -50.0f,   50.0f, 0.0f, 1.0f,// 3
+
+          150.0f,  -50.0f, 0.0f, 0.0f,// 0
+          250.0f,  -50.0f, 1.0f, 0.0f,// 1
+          250.0f,   50.0f, 1.0f, 1.0f,// 2
+          150.0f,   50.0f, 0.0f, 1.0f// 3
         };
 
         unsigned int indices[] = {
             0, 1, 2,
-            2, 3, 0
+            2, 3, 0,
+            4, 5, 6,
+            6, 7, 4
         };
 
         GLCall(glEnable(GL_BLEND));
@@ -65,7 +72,7 @@ int main(void)
         // generate and bind array buffer, and load postion data to the buffer
         // enable VertexAttribArray to specify layout of positon data
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+        VertexBuffer vb(positions, 8 * 4 * sizeof(float));
 
         VertexBufferLayout layout;
         layout.Push<float>(2);
@@ -74,7 +81,7 @@ int main(void)
 
         // draw rectangle is actually draw 2 triangle using 2 * 3 vertices
         // generate and bind new element array buffer, and load indices data to the buffer
-        IndexBuffer ib(indices, 6);
+        IndexBuffer ib(indices, 12);
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f); // vertices projection position 
         glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0)); // camera view position
@@ -87,10 +94,10 @@ int main(void)
         texture.Bind();
         shader.SetUniform4i("u_Texture", 0);
 
-        va.Unbind();
+        /*va.Unbind();
         vb.Unbind();
         ib.Unbind();
-        shader.Unbind();
+        shader.Unbind();*/
 
         Renderer renderer;
 
@@ -99,7 +106,7 @@ int main(void)
         ImGui::StyleColorsDark();
 
         glm::vec3 translationA(200, 200, 0);
-        glm::vec3 translationB(400, 200, 0);
+        //glm::vec3 translationB(400, 200, 0);
 
         float r_ch = 0.0f;
         float increment = 0.05f;
@@ -121,12 +128,12 @@ int main(void)
                 renderer.Draw(va, ib, shader);
             }
 
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB); // model object position
-                glm::mat4 mvp = proj * view * model;
-                shader.SetUniformMAT4f("u_MVP", mvp);
-                renderer.Draw(va, ib, shader);
-            }
+            //{
+            //    glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB); // model object position
+            //    glm::mat4 mvp = proj * view * model;
+            //    shader.SetUniformMAT4f("u_MVP", mvp);
+            //    renderer.Draw(va, ib, shader);
+            //}
 
             shader.SetUniform4f("u_Color", r_ch, 0.3f, 0.8f, 1.0f);
 
@@ -137,7 +144,7 @@ int main(void)
 
             {
                 ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f 
-                ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
+                //ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             }
 
